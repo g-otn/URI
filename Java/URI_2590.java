07f196e97a40c0c8e6065d9a27beb92ea9f5e8fa
g-otn <g-otn@hotmail.com>
2020-01-22 03:05:07 -0300

    create: URI_2590.java with initial submission

---
+++ Java/URI_2590.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// g-otn
+// URI_2590
+// Sete
+
+public class URI_2590 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Cheat sheet / testing
+//        for (int i = 0; i < 50; i++) {
+//            System.out.println("i: " + i + "\timod7: " + (i % 7) + "\ti/7: " + (i/7) + "\tteste: " + (i%8) + "\túltimo: " + Long.valueOf((long)Math.pow(7, i)).toString().charAt(Long.valueOf((long)Math.pow(7, i)).toString().length() - 1) + "\t7^i: " + (long) (Math.pow(7, i)));
+//        }
+
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+
+        int numeroDeInstancias = Integer.parseInt(leitor.readLine());
+
+        for (int i = 0; i < numeroDeInstancias; i++) {
+
+            int expoente = Integer.parseInt(leitor.readLine()); // Integer.MAX_VALUE > 10^9
+            int fatiaDaPizza = expoente % 8;
+
+            if (fatiaDaPizza == 0) {
+                System.out.println("1");
+            } else if (fatiaDaPizza == 1) {
+                System.out.println("7");
+            } else if (fatiaDaPizza == 2) {
+                System.out.println("9");
+            } else if (fatiaDaPizza == 3) {
+                System.out.println("3");
+            } else if (fatiaDaPizza == 4) {
+                System.out.println("1");
+            } else if (fatiaDaPizza == 5) {
+                System.out.println("7");
+            } else if (fatiaDaPizza == 6) {
+                System.out.println("9");
+            } else if (fatiaDaPizza == 7) {
+                System.out.println("3");
+            }
+        }
+
+    }
+
+}
